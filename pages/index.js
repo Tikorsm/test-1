f5d1b22fb5341d652bbcb0d80d3447a57af99f83
Tikorsm <95771559+Tikorsm@users.noreply.github.com>
2023-10-17 08:28:31 -0300

    commit1

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import {useState} from "react";
+
+
+function Home() {
+    return( 
+      <div>
+        <h2>Home</h2>
+        <Contador />
+      </div>
+    
+    )
+
+
+}
+
+
+
+function Contador() {
+    const [contador, setContador] = useState();
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    return(
+         <div>
+          <div>{contador}</div>
+          <button onClick={adicionarContador}>Adicionar</button>
+         </div>
+    )
+}
+
+export default Home
